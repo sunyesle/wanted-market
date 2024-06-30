8e22d9e0dfcd4c36fcff5b67ce05b54aab5bdf1a
sunyesle <sunyesle@gmail.com>
2024-06-30 23:28:28 +0900

    refector: 로그인한 회원 아이디를 가져오는 커스텀어노테이션 추가

---
+++ src/main/java/com/sunyesle/wanted_market/global/security/LoginMember.java
@@ -0,0 +1,14 @@
+package com.sunyesle.wanted_market.global.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : id")
+public @interface LoginMember {
+}
